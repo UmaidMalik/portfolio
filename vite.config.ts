aac0aa2a8f76fb3f9128e054f2bf4e929bde5fb1
Umaid Malik <umaid54321@hotmail.com>
2026-06-12 00:22:13 -0400

    added ci/cd

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
-import path from "path"
+import path from "node:path"
 import tailwindcss from "@tailwindcss/vite"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: "/",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
